92d281bec76d2638683bd2bbd372d768c58260ac
Jakub Chmielewski <jakubb.chmielewskii@gmail.com>
2019-01-29 12:52:39 +0100

    Add style for header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,14 +3,14 @@ import { Link } from 'react-router-dom';
 
 const Header = () => (
   <header>
-    <Link to="/" >Main</Link>
-    <Link to="/firstPage" >container + presentational component</Link>
-    <Link to="/secondPage" >render prop</Link>
-    <Link to="/thirdPage" >function as children</Link>
-    <Link to="/fourthPage" >Higher Order Component</Link>
-    <Link to="/fifthPage" >Refs</Link>
-    <Link to="/sixthPage" >Component and PureComponent</Link>
-    <Link to="/seventhPage" >Selectors</Link>
+    <Link to="/" className="link1">Main</Link>
+    <Link to="/firstPage" className="link2" >container + presentational component</Link>
+    <Link to="/secondPage" className="link3" >render prop</Link>
+    <Link to="/thirdPage" className="link4" >function as children</Link>
+    <Link to="/fourthPage" className="link5" >Higher Order Component</Link>
+    <Link to="/fifthPage" className="link6" >Refs</Link>
+    <Link to="/sixthPage" className="link7" >Component and PureComponent</Link>
+    <Link to="/seventhPage" className="link8" >Selectors</Link>
   </header>
 );
 
